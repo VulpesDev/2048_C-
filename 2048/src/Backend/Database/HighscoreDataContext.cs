3d6cc6a4a0f5898ce535263555759dca6025be1a
Kayakaga <baijidevelop@gmail.com>
2024-06-16 23:22:15 +0300

    little tweaks

--- 2048/src/Backend/Database/HighscoreDataContext.cs
+++ 2048/src/Backend/Database/HighscoreDataContext.cs
@@ -11,15 +11,5 @@ namespace _2048.src.Backend.Database
         }
 
         public DbSet<Highscore> Highscores { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                var connectionString = "server=localhost;port=3306;database=2048_VulpesDev;user=root;password=123321";
-
-                optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 28)));
-            }
-        }
     }
 }

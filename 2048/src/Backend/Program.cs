3ee6ce865204a08c8ef7866db8c982ad4aaec851
Kayakaga <baijidevelop@gmail.com>
2024-06-17 16:21:58 +0300

    finally connecting to an online database

--- 2048/src/Backend/Program.cs
+++ 2048/src/Backend/Program.cs
@@ -1,8 +1,10 @@
 ﻿using _2048.src;
 using _2048.src.Backend;
 using _2048.src.Backend.Database;
+using dotenv.net;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace _2048
@@ -29,11 +31,24 @@ namespace _2048
         {
             if (Context == null)
             {
-                var optionsBuilder = new DbContextOptionsBuilder<HighscoreDataContext>();
-                optionsBuilder.UseMySql("server=localhost;port=3306;database=2048_VulpesDev;user=root;password=123321",
-                                        new MySqlServerVersion(new Version(8, 0, 28)));
+                string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                string projectRootDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(baseDirectory, @"..\.."));
+                Console.WriteLine($"Project root directory: {Path.Combine(projectRootDirectory, ".env")}");
 
-                Context = new HighscoreDataContext(optionsBuilder.Options);
+                DotEnv.Fluent()
+                    .WithExceptions()
+                    .WithEnvFiles(Path.Combine(projectRootDirectory, ".env"))
+                    .Load();
+                string connection_string = DotEnv.Fluent()
+                    .WithEnvFiles(Path.Combine(projectRootDirectory, ".env"))
+                    .Read()["2048_CON_SECRET_VULPESDEV"];
+                Console.WriteLine($"My secret value is: {connection_string}");
+                if (Context == null)
+                {
+                    var optionsBuilder = new DbContextOptionsBuilder<HighscoreDataContext>();
+                    optionsBuilder.UseMySql(connection_string, new MySqlServerVersion(new Version(8, 0, 28)));
+                    Context = new HighscoreDataContext(optionsBuilder.Options);
+                }
             }
         }
     }
